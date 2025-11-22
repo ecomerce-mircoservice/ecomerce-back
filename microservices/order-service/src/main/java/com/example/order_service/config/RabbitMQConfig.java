39c8602e49d728062be202f1aa36dc63ac5e29e0
Ibrahim Goumrane <goumrane.ibrahim@ensam-casa.ma>
2025-11-22 19:46:44 +0100

    feat: Add Docker support and update RabbitMQ configuration for order and product services

--- microservices/order-service/src/main/java/com/example/order_service/config/RabbitMQConfig.java
+++ microservices/order-service/src/main/java/com/example/order_service/config/RabbitMQConfig.java
@@ -1,10 +1,13 @@
 package com.example.order_service.config;
 
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -50,7 +53,7 @@ public class RabbitMQConfig {
 
     @Bean
     public MessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
+        return new SimpleMessageConverter();
     }
 
     @Bean
