7239e235da75c63f59d1f7505aa254831f76f3c4
Ibrahim Goumrane <goumrane.ibrahim@ensam-casa.ma>
2025-12-15 21:49:17 +0100

    feat: Update PostgreSQL setup to use env_file and ensure databases are created only if they do not exist

--- microservices/init-postgres.sh
+++ microservices/init-postgres.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE $DB_NAME_AUTH;
-    CREATE DATABASE $DB_NAME_PRODUCT;
-    CREATE DATABASE $DB_NAME_ORDER;
+# Create databases only if they do not exist
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  -- Auth DB
+  SELECT 'CREATE DATABASE $DB_NAME_AUTH'
+  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$DB_NAME_AUTH')\gexec
+
+  -- Product DB
+  SELECT 'CREATE DATABASE $DB_NAME_PRODUCT'
+  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$DB_NAME_PRODUCT')\gexec
+
+  -- Order DB
+  SELECT 'CREATE DATABASE $DB_NAME_ORDER'
+  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$DB_NAME_ORDER')\gexec
 EOSQL
