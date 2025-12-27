4d4012f51ec0aae502d1cc1416fc20bbaa9799b7
Ibrahim Goumrane <goumrane.ibrahim@ensam-casa.ma>
2025-12-27 15:44:11 +0100

    feat: Add initial application properties and a basic Spring Boot test class.

--- microservices/product-service/src/test/java/com/example/product_service/ProductServiceApplicationTests.java
+++ microservices/product-service/src/test/java/com/example/product_service/ProductServiceApplicationTests.java
@@ -1,11 +1,12 @@
 package com.example.product_service;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
 @SpringBootTest
+@ActiveProfiles("test")
 class ProductServiceApplicationTests {
 
 	@Test
